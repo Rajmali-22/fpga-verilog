5509f037c4ea9e12e8e5430e5ad0cac42112b69c
Rajmali-22 <22.raj.mali@gmail.com>
2025-03-28 12:50:48 +0530

    Add files via upload

---
+++ on_1.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.02.2025 11:02:43
+// Design Name: 
+// Module Name: on_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module on_1(
+input sw,
+output led
+
+    );
+    assign led=sw;
+    
+endmodule
